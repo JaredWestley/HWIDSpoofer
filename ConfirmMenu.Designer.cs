71d5ed70d2aa125289fdc81c65460064d14d1fd9
JaredWestley <105741779+JaredWestley@users.noreply.github.com>
2022-08-17 16:46:20 +0100

    Add files via upload

---
+++ ConfirmMenu.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace KeyAuth
+{
+    partial class ConfirmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfirmMenu));
+            this.siticonePictureBox1 = new Siticone.UI.WinForms.SiticonePictureBox();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneControlBox3 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneControlBox4 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticonePanel1 = new Siticone.UI.WinForms.SiticonePanel();
+            this.siticoneRoundedButton1 = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneRoundedButton2 = new Siticone.UI.WinForms.SiticoneRoundedButton();
+            this.siticoneLabel4 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticoneDragControl2 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticoneDragControl3 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // siticonePictureBox1
+            // 
+            this.siticonePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.siticonePictureBox1.Image = global::Loader.Properties.Resources.GitHub_Mark;
+            this.siticonePictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.siticonePictureBox1.Name = "siticonePictureBox1";
+            this.siticonePictureBox1.ShadowDecoration.Parent = this.siticonePictureBox1;
+            this.siticonePictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.siticonePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.siticonePictureBox1.TabIndex = 39;
+            this.siticonePictureBox1.TabStop = false;
+            this.siticonePictureBox1.Click += new System.EventHandler(this.siticonePictureBox1_Click);
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.siticoneLabel1.Location = new System.Drawing.Point(150, 12);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(134, 25);
+            this.siticoneLabel1.TabIndex = 38;
+            this.siticoneLabel1.Text = "HWID Spoofer";
+            // 
+            // siticoneControlBox3
+            // 
+            this.siticoneControlBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox3.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneControlBox3.BorderRadius = 5;
+            this.siticoneControlBox3.ControlBoxType = Siticone.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.siticoneControlBox3.FillColor = System.Drawing.Color.Transparent;
+            this.siticoneControlBox3.HoveredState.Parent = this.siticoneControlBox3;
+            this.siticoneControlBox3.IconColor = System.Drawing.SystemColors.ControlDark;
+            this.siticoneControlBox3.Location = new System.Drawing.Point(360, 12);
+            this.siticoneControlBox3.Name = "siticoneControlBox3";
+            this.siticoneControlBox3.ShadowDecoration.Parent = this.siticoneControlBox3;
+            this.siticoneControlBox3.Size = new System.Drawing.Size(28, 19);
+            this.siticoneControlBox3.TabIndex = 40;
+            this.siticoneControlBox3.Click += new System.EventHandler(this.siticoneControlBox3_Click);
+            // 
+            // siticoneControlBox4
+            // 
+            this.siticoneControlBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox4.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneControlBox4.BorderRadius = 5;
+            this.siticoneControlBox4.FillColor = System.Drawing.Color.Transparent;
+            this.siticoneControlBox4.HoveredState.FillColor = System.Drawing.Color.Red;
+            this.siticoneControlBox4.HoveredState.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox4.HoveredState.Parent = this.siticoneControlBox4;
+            this.siticoneControlBox4.IconColor = System.Drawing.Color.Red;
+            this.siticoneControlBox4.Location = new System.Drawing.Point(394, 12);
+            this.siticoneControlBox4.Name = "siticoneControlBox4";
+            this.siticoneControlBox4.ShadowDecoration.Parent = this.siticoneControlBox4;
+            this.siticoneControlBox4.Size = new System.Drawing.Size(28, 19);
+            this.siticoneControlBox4.TabIndex = 41;
+            this.siticoneControlBox4.Click += new System.EventHandler(this.siticoneControlBox4_Click);
+            // 
+            // siticonePanel1
+            // 
+            this.siticonePanel1.Location = new System.Drawing.Point(-1, -4);
+            this.siticonePanel1.Name = "siticonePanel1";
+            this.siticonePanel1.ShadowDecoration.Parent = this.siticonePanel1;
+            this.siticonePanel1.Size = new System.Drawing.Size(434, 41);
+            this.siticonePanel1.TabIndex = 42;
+            // 
+            // siticoneRoundedButton1
+            // 
+            this.siticoneRoundedButton1.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.siticoneRoundedButton1.BorderThickness = 1;
+            this.siticoneRoundedButton1.CheckedState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.CustomImages.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(127)))), ((int)(((byte)(243)))));
+            this.siticoneRoundedButton1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneRoundedButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton1.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.siticoneRoundedButton1.HoveredState.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Location = new System.Drawing.Point(31, 84);
+            this.siticoneRoundedButton1.Name = "siticoneRoundedButton1";
+            this.siticoneRoundedButton1.ShadowDecoration.Parent = this.siticoneRoundedButton1;
+            this.siticoneRoundedButton1.Size = new System.Drawing.Size(176, 46);
+            this.siticoneRoundedButton1.TabIndex = 48;
+            this.siticoneRoundedButton1.Text = "Yes";
+            this.siticoneRoundedButton1.Click += new System.EventHandler(this.siticoneRoundedButton1_Click);
+            // 
+            // siticoneRoundedButton2
+            // 
+            this.siticoneRoundedButton2.BorderColor = System.Drawing.Color.DodgerBlue;
+            this.siticoneRoundedButton2.BorderThickness = 1;
+            this.siticoneRoundedButton2.CheckedState.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.CustomImages.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(127)))), ((int)(((byte)(243)))));
+            this.siticoneRoundedButton2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneRoundedButton2.ForeColor = System.Drawing.Color.White;
+            this.siticoneRoundedButton2.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.siticoneRoundedButton2.HoveredState.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.Location = new System.Drawing.Point(227, 84);
+            this.siticoneRoundedButton2.Name = "siticoneRoundedButton2";
+            this.siticoneRoundedButton2.ShadowDecoration.Parent = this.siticoneRoundedButton2;
+            this.siticoneRoundedButton2.Size = new System.Drawing.Size(176, 46);
+            this.siticoneRoundedButton2.TabIndex = 49;
+            this.siticoneRoundedButton2.Text = "No";
+            this.siticoneRoundedButton2.Click += new System.EventHandler(this.siticoneRoundedButton2_Click);
+            // 
+            // siticoneLabel4
+            // 
+            this.siticoneLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.siticoneLabel4.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.siticoneLabel4.Location = new System.Drawing.Point(65, 51);
+            this.siticoneLabel4.Name = "siticoneLabel4";
+            this.siticoneLabel4.Size = new System.Drawing.Size(323, 18);
+            this.siticoneLabel4.TabIndex = 50;
+            this.siticoneLabel4.Text = "You must restart your PC, would you like to do so now?";
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this.siticonePictureBox1;
+            // 
+            // siticoneDragControl2
+            // 
+            this.siticoneDragControl2.TargetControl = this.siticoneLabel1;
+            // 
+            // siticoneDragControl3
+            // 
+            this.siticoneDragControl3.TargetControl = this.siticonePanel1;
+            // 
+            // ConfirmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.ClientSize = new System.Drawing.Size(434, 142);
+            this.Controls.Add(this.siticoneLabel4);
+            this.Controls.Add(this.siticoneRoundedButton2);
+            this.Controls.Add(this.siticoneRoundedButton1);
+            this.Controls.Add(this.siticonePictureBox1);
+            this.Controls.Add(this.siticoneLabel1);
+            this.Controls.Add(this.siticoneControlBox3);
+            this.Controls.Add(this.siticoneControlBox4);
+            this.Controls.Add(this.siticonePanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ConfirmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ConfirmMenu";
+            ((System.ComponentModel.ISupportInitialize)(this.siticonePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticonePictureBox siticonePictureBox1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox3;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox4;
+        private Siticone.UI.WinForms.SiticonePanel siticonePanel1;
+        private Siticone.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton1;
+        private Siticone.UI.WinForms.SiticoneRoundedButton siticoneRoundedButton2;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel4;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl2;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl3;
+    }
+}
---
+++ Global.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyAuth
+{
+    class Global
+    {
+        public static bool PCName = false;
+        public static bool GUID = false;
+        public static bool HWIDserial = false;
+        public static bool ProductID = false;
+        public static bool MacAddress = false;
+        public static bool InstallTime = false;
+        public static bool InstallDate = false;
+        public static bool HwProfileGUID = false;
+
+        public static void Restart()
+        {
+            StartShutDown("-f -r -t 5");
+        }
+
+        private static void StartShutDown(string param)
+        {
+            ProcessStartInfo proc = new ProcessStartInfo();
+            proc.FileName = "cmd";
+            proc.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Arguments = "/C shutdown " + param;
+            Process.Start(proc);
+        }
+    }
+}
